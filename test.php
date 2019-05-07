097256cdec61e298e6ec9e6d6f56a855e41b550c
Duong Duy Minh <minh@dirox.net>
2019-05-07 23:48:35 +0700

    initial

---
+++ test.php
@@ -0,0 +1,32 @@
+<?php
+
+$fnUsr = fopen("PARAMETER1.usr","r");
+$fnCsv = fopen('/tmp/file.csv', 'w');
+
+
+while(! feof($fnUsr))  {
+    $line = fgets($fnUsr);
+
+    if (!preg_match("/^\d+\..+/", $line)){
+    	continue;
+    }
+    	
+    //1. Replace 02 tab by one
+    $line = preg_replace("/\t{2}/", "\t", $line);
+
+    //2. read csv text based on tab can 
+    $fields = str_getcsv($line, "\t");
+
+    //3. Trimming
+    $trimmed_spaces = array_map('trim', $fields);
+
+    //4. Convert back to csv with default delimiter and enclosure.
+    fputcsv($fnCsv, $trimmed_spaces,',','"');
+    
+}
+
+fclose($fnUsr);
+fclose($fnCsv);
+
+//Debug:
+echo file_get_contents("/tmp/file.csv");
